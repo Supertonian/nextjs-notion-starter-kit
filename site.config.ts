b6298899bd355913a2196fdc84d5a661130cf245
이창윤 <brownbearpower@gmail.com>
2022-04-24 14:10:10 +0900

    fix: remove undefined

--- site.config.ts
+++ site.config.ts
@@ -27,12 +27,6 @@ export default siteConfig({
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
-  // image CDN host to proxy all image requests through (optional)
-  // NOTE: this requires you to set up an external image proxy
-  imageCDNHost: null,
-
-  // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: null,
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
